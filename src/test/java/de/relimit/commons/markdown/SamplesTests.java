5ebaab107ee6f26a3f17a55173cae7c4725b0651
AndreaUhl <andrea.uhl@relimit.de>
2024-01-08 08:36:41 +0100

    #5 Testmethod added to compare keys to properties

--- src/test/java/de/relimit/commons/markdown/SamplesTests.java
+++ src/test/java/de/relimit/commons/markdown/SamplesTests.java
@@ -2,10 +2,14 @@ package de.relimit.commons.markdown;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -41,6 +45,28 @@ public class SamplesTests {
 					}
 					return true;
 				}).collect(Collectors.toList());
+	}
+
+	@TestFactory
+	Stream<DynamicTest> propertyKeysFound_CompareKeysToPropertyKeys_NoneMissing() {
+		return methods().stream().map(m -> {
+			final Properties props = new Properties();
+			final ClassLoader loader = Thread.currentThread().getContextClassLoader();
+			final InputStream stream = loader.getResourceAsStream(Samples.PROPERTIES_FILE);
+			try {
+				props.load(stream);
+			} catch (final IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			final Sample sample = m.getAnnotation(Sample.class);
+			final String propertiesKey = Samples.PROPERTY_KEY_NAMESPACE + "." + sample.key() + "."
+					+ Samples.PROPERTY_KEY_SUFFIX_HEADING;
+			final String heading = props.getProperty(propertiesKey);
+			System.out.println(propertiesKey + ", " + heading);
+			return DynamicTest.dynamicTest(m.getName(), () -> assertNotNull(heading));
+		});
+
 	}
 
 	@Test
